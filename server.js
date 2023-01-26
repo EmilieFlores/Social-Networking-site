152c2721e2b2890d3a5d10f65b2d4afbe612f784
EmilieFlores <codingtime0000@gmail.com>
2023-01-25 18:47:42 -0600

    update

--- server.js
+++ server.js
@@ -5,5 +5,40 @@ const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 const methodOverride = require("method-override");
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
 
+
+//Use .env file in config folder
+require("dotenv").config({ path: "./config/.env"});
+
+//Passport config 
+require("./config/passport")(passport);
+
+//connect to Database
+connectDB();
+
+//Using EJS for views 
+app.set("view engine", "ejs");
+
+//static folder
+app.use(express.static("public"));
+
+//Body parsing 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//logging
+app.use(logger("dev"));
+
+//Use forms for put/delete 
+app.use(methodOverride("_method"));
+
+//Setup sessions -stored in MongoDb 
+app.use(
+    
+)
 
