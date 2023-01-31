b8a1020e91aa4e0cd9e0fe04081e25818d7b861f
EmilieFlores <codingtime0000@gmail.com>
2023-01-31 15:47:03 -0600

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,8 +44,39 @@ exports.postLogin = (req, res, next) => {
     })(requ, res, next);
 };
 
+exports.logout = (req, res) => {
+    req.logout(() => {
+        console.log('User has logged out.')
+    })
+    req.session.destroy((err) => {
+        if(err)
+        console.log("Error : Failed to destroy the session during logout.", err)
+        req.user = null;
+        res.redirect("/");
+    });
+};
 
+exports.getSignup = (req, res) => {
+    if(req.user) {
+        return res.redirct("/profile");
+    }
+    res.render("signup", {
+        title: "Create Account",
+    });
+};
 
+exports.postSignup = (req, res, next) => {
+    const validationErrors = [];
+    if (!validator.isEmail(req.body.email))
+        validationErrors.push({msg: "Please enter a vaild email address."});
+    if(!validator.isLength(req.body.password, {min: 8}))
+    validationErrors.push({
+        msg: "Password must be at least 8 characters long",
+    });
+    if(req.body.password !== req.body.confirmPassword)
+        validationErrors.push({msg: "Passwords do not match"});
+    
+}
 
 
 
