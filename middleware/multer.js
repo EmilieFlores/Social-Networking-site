62ddf02761acfc75897f7e07bbd097da87a84712
EmilieFlores <codingtime0000@gmail.com>
2023-02-06 11:44:43 -0600

    update

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,7 +5,7 @@ module.exports = multer({
     storage: multer.diskStorage({}),
     fileFilter: (req, file, cd) => {
         let ext = path.extname(file.originalname);
-        if(ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png' && ext !== '.JPG') {
+        if(ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png' && ext !== '.JPG'&& ext !== '.webp') {
             cd(new Error("File type is not supported"), false);
             return;
         }
